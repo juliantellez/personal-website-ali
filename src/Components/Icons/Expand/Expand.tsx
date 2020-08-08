393fb5a4de87d7223131dd10dfea89a7dbcb3107
Julian <juliantellezmendez@gmail.com>
2020-08-08 13:48:55 +0100

    fix: lint

--- src/Components/Icons/Expand/Expand.tsx
+++ src/Components/Icons/Expand/Expand.tsx
@@ -1,12 +1,12 @@
 // tslint:disable:max-line-length
 
-import * as React from "react";
+import * as React from 'react';
 
-interface ExpandProps {
-    className: string
+interface IExpandProps {
+    className: string;
 }
 
-const Expand: React.FC<ExpandProps> = (props) => (
+const Expand: React.FC<IExpandProps> = (props) => (
   <svg {...props} viewBox="0 0 298.667 298.667">
     <path d="M42.667 192H0v106.667h106.667V256h-64zM0 106.667h42.667v-64h64V0H0zM192 0v42.667h64v64h42.667V0zM256 256h-64v42.667h106.667V192H256z" />
   </svg>
--- src/Pages/Home/Components/Content/Content.tsx
+++ src/Pages/Home/Components/Content/Content.tsx
@@ -1,23 +1,24 @@
 // tslint:disable:max-line-length
 
-import * as React from "react";
+import * as React from 'react';
 
-import * as styles from "./Content.scss";
-import Expand from "../../../../Components/Icons/Expand";
+import Expand from '../../../../Components/Icons/Expand';
+
+import * as styles from './Content.scss';
 
 const projectUrlList: string[] = [
-    "/assets/images/projects/birki_website.png",
-  "/assets/images/projects/santa_murs_webiste.png",
-  "/assets/images/projects/free_will_communitiy_website.png",
-  "/assets/images/projects/movie-bucket.png",
-  "/assets/images/projects/amigos.png",
-  "/assets/images/projects/laviston.png",
-  "/assets/images/projects/drills_guru_section.png",
-  "/assets/images/projects/cindy.png",
-  "/assets/images/projects/drills_guru_website.png",
-  "/assets/images/projects/latinos_in_london.png",
-  "/assets/images/projects/analytics_ui.png",
-  "/assets/images/projects/butter_sketch.png",
+    '/assets/images/projects/birki_website.png',
+  '/assets/images/projects/santa_murs_webiste.png',
+  '/assets/images/projects/free_will_communitiy_website.png',
+  '/assets/images/projects/movie-bucket.png',
+  '/assets/images/projects/amigos.png',
+  '/assets/images/projects/laviston.png',
+  '/assets/images/projects/drills_guru_section.png',
+  '/assets/images/projects/cindy.png',
+  '/assets/images/projects/drills_guru_website.png',
+  '/assets/images/projects/latinos_in_london.png',
+  '/assets/images/projects/analytics_ui.png',
+  '/assets/images/projects/butter_sketch.png'
 ];
 
 const Content = () => {
@@ -27,7 +28,7 @@ const Content = () => {
         {projectUrlList.map((projectUrl, key) => {
           const style = {
             gridArea: `item-${key + 1}`,
-            backgroundImage: `url(${projectUrl})`,
+            backgroundImage: `url(${projectUrl})`
           };
 
           return (
