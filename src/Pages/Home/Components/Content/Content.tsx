d1113d6ceb0fcdd0c0ff71732f829073be05010e
Julian <juliantellezmendez@gmail.com>
2020-08-08 12:45:00 +0100

    feature: add home page image gallery

--- src/Pages/Home/Components/Content/Content.tsx
+++ src/Pages/Home/Components/Content/Content.tsx
@@ -1,22 +1,68 @@
 // tslint:disable:max-line-length
 
-import * as React from 'react';
-import {Link} from 'react-router-dom';
+import * as React from "react";
+// import {Link} from 'react-router-dom';
 
-import Logo from '../../../../Components/Icons/Logo';
+// import Logo from '../../../../Components/Icons/Logo';
 
-import * as styles from './Content.scss';
+import * as styles from "./Content.scss";
+import Expand from "../../../../Components/Icons/Expand";
+
+// const getWork = (link, img) => (
+//     <Link className={styles.contentWorksWork} to={link}>
+//         <img className={styles.contentWorksWorkImg} src={img} />
+//     </Link>
+// );
+
+const projectUrlList: string[] = [
+  "/assets/images/projects/santa_murs_webiste.png",
+  "/assets/images/projects/amigos.png",
+  "/assets/images/projects/birki_website.png",
+  "/assets/images/projects/free_will_communitiy_website.png",
+  "/assets/images/projects/movie-bucket.png",
+  "/assets/images/projects/laviston.png",
+  "/assets/images/projects/drills_guru_section.png",
+  "/assets/images/projects/latinos_in_london.png",
+  "/assets/images/projects/butter_sketch.png",
+  "/assets/images/projects/calendar.png",
+  "/assets/images/projects/drills_guru_logo.png",
+  "/assets/images/projects/analytics_ui.png",
+  "/assets/images/projects/drills_guru_website.png",
+  "/assets/images/projects/cindy.png",
+  "/assets/images/projects/news_app.png",
+  "/assets/images/projects/news_app_reminder_1.png",
+  "/assets/images/projects/news_app_reminder_2.png",
+  "/assets/images/projects/news_app_story.png",
+  "/assets/images/projects/article_screen.png",
+];
 
 const Content = () => {
-    const getWork = (link, img) => (
-        <Link className={styles.contentWorksWork} to={link}>
-            <img className={styles.contentWorksWorkImg} src={img} />
-        </Link>
-    );
+  const galleryStyle = {
+    // gridTemplateAreas: "item-1 item-2 item-3 item-4"
+  };
 
-    return (
-        <div className={styles.content}>
-            <section className={styles.contentSection}>
+  return (
+    <div className={styles.container}>
+      <div style={galleryStyle} className={styles.gallery}>
+        {projectUrlList.map((projectUrl) => {
+          const style = {
+            // gridArea: `item-${key + 1}`,
+            backgroundImage: `url(${projectUrl})`,
+          };
+
+          return (
+            <a
+              key={projectUrl}
+              href={projectUrl}
+              style={style}
+              className={styles.galleryAnchor}
+            >
+              <Expand className={styles.galleryAnchorIcon} />
+            </a>
+          );
+        })}
+      </div>
+      {/* <section className={styles.contentSection}>
                 <Logo />
                 <div className={styles.contentSectionText}>
                     Hi I’m Ali, welcome to my online portfolio.
@@ -54,9 +100,9 @@ const Content = () => {
                 <div>Let's Talk,</div>
                 <div>I'm always looking for new and exciting projects!</div>
                 <Link to="/contact">Contact Me Here</Link>
-            </section>
-        </div>
-    );
+            </section> */}
+    </div>
+  );
 };
 
 export default Content;
